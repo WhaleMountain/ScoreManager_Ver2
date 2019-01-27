fe6501bc5a5353b5ea7066aff027d01e7ed719e0
Tomohiro Inoue <e175762@ie.u-ryukyu.ac.jp>
2019-01-27 15:03:44 +0900

    fix main color

--- ScoreManager/ScoreManager/ViewController.swift
+++ ScoreManager/ScoreManager/ViewController.swift
@@ -137,6 +137,27 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
             selector: #selector(delSave),
             name:UIApplication.didEnterBackgroundNotification,
             object: nil)
+        
+        //グラデーションの開始色
+        let topColor = UIColor(red: 156/255, green: 39/255, blue: 176/255, alpha:1)
+        //グラデーションの中間色
+        let borderColor = UIColor(red: 32/255, green: 76/255, blue: 255/255, alpha:1)
+        //グラデーションの開始色
+        let bottomColor = UIColor(red: 58/255, green: 255/255, blue: 217/255, alpha:1)
+        
+        //グラデーションの色を配列で管理
+        let gradientColors: [CGColor] = [topColor.cgColor,borderColor.cgColor, bottomColor.cgColor]
+        
+        //グラデーションレイヤーを作成
+        let gradientLayer: CAGradientLayer = CAGradientLayer()
+        
+        //グラデーションの色をレイヤーに割り当てる
+        gradientLayer.colors = gradientColors
+        //グラデーションレイヤーをスクリーンサイズにする
+        gradientLayer.frame = self.view.bounds
+        
+        //グラデーションレイヤーをビューの一番下に配置
+        self.view.layer.insertSublayer(gradientLayer,at: 0)
     }
 
 
